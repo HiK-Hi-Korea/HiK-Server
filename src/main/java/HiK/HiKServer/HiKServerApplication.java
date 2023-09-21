e6eeea20a42f63f791ac15526537658c58d1069c
bbx8216 <bbx8216@gmail.com>
2023-09-21 13:31:04 +0900

    init

---
+++ src/main/java/HiK/HiKServer/HiKServerApplication.java
@@ -0,0 +1,13 @@
+package HiK.HiKServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiKServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiKServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HiK/HiKServer/HiKServerApplicationTests.java
@@ -0,0 +1,13 @@
+package HiK.HiKServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiKServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
