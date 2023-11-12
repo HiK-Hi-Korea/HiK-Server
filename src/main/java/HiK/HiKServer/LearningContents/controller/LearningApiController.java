e2c15647797380ea9c923ca8a01ede963267e446
bbx8216 <bbx8216@gmail.com>
2023-11-12 09:58:51 +0900

    chore: stash

---
+++ src/main/java/HiK/HiKServer/LearningContents/controller/LearningApiController.java
@@ -0,0 +1,19 @@
+package HiK.HiKServer.LearningContents.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LearningApiController {
+//    @Autowired
+//    private TranslationService translationService;
+//
+//    @PostMapping("/trans")
+//    public ResponseEntity<Sentence> translation(@RequestBody TranslationForm translationForm) throws IOException {
+//        System.out.println("translation 시작");
+//        Sentence translatedSentence = translationService.translation(translationForm);
+//        return (translatedSentence != null) ? ResponseEntity.status(HttpStatus.OK).body(translatedSentence):
+//                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//    }
+}
---
+++ src/test/java/HiK/HiKServer/repository/LearningContentsTest.java
@@ -0,0 +1,29 @@
+package HiK.HiKServer.repository;
+
+import HiK.HiKServer.LearningContents.repository.LearningContentRepository;
+import HiK.HiKServer.entity.LearningContent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class LearningContentsTest {
+    @Autowired
+    private LearningContentRepository learningContentRepository;
+
+    @Test
+    public void findSimilarSentenceTest(){
+        List<LearningContent> data = learningContentRepository.findAll();
+        System.out.println("-----------------");
+        for (LearningContent d: data){
+            System.out.println("learning Content: "+ d.toString());
+        }
+        System.out.println("-----------------");
+
+        assertNotNull(data);
+    }
+}
