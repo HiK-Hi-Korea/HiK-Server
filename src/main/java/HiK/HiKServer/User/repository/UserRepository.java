81dddb3d164de3e4290696cc50e156314f46b9e2
bbx8216 <bbx8216@gmail.com>
2023-11-27 22:20:24 +0900

    delete: test코드 삭제

--- src/main/java/HiK/HiKServer/User/repository/UserRepository.java
+++ src/main/java/HiK/HiKServer/User/repository/UserRepository.java
@@ -4,7 +4,4 @@ import HiK.HiKServer.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-//    boolean existsByLoginId(String loginId);
-//    boolean existsByNickname(String nickname);
-//    Optional<User> findByLoginId(String loginId);
 }
--- src/test/java/HiK/HiKServer/repository/UserRepositoryTest.java
+++ src/test/java/HiK/HiKServer/repository/UserRepositoryTest.java
@@ -1,32 +1,32 @@
-package HiK.HiKServer.repository;
-
-import HiK.HiKServer.User.repository.UserRepository;
-import HiK.HiKServer.User.domain.User;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest
-public class UserRepositoryTest {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    public void testSaveAndFind() {
-        List<User> all = userRepository.findAll();
-        System.out.println("--------------------------");
-        for(User user: all){
-            System.out.println(user.getId());
-        }
-        System.out.println("--------------------------");
-
-        User foundUser = userRepository.findById("1").orElse(null);
-        // then
-        assertNotNull(foundUser);
-        // assertNotNull(all);
-    }
-}
+//package HiK.HiKServer.repository;
+//
+//import HiK.HiKServer.User.repository.UserRepository;
+//import HiK.HiKServer.User.domain.User;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//@SpringBootTest
+//public class UserRepositoryTest {
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Test
+//    public void testSaveAndFind() {
+//        List<User> all = userRepository.findAll();
+//        System.out.println("--------------------------");
+//        for(User user: all){
+//            System.out.println(user.getId());
+//        }
+//        System.out.println("--------------------------");
+//
+//        User foundUser = userRepository.findById("1").orElse(null);
+//        // then
+//        assertNotNull(foundUser);
+//        // assertNotNull(all);
+//    }
+//}
