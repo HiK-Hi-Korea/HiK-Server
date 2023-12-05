18116699da6b9e0ae6b46448acca4710ccab8c89
bbx8216 <bbx8216@gmail.com>
2023-12-05 13:33:59 +0900

    feat: User set메소드 추가

--- src/main/java/HiK/HiKServer/User/controller/UserApiController.java
+++ src/main/java/HiK/HiKServer/User/controller/UserApiController.java
@@ -7,12 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -38,9 +36,30 @@ public class UserApiController {
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
-    @GetMapping("/{userId}/learning-contents")
-    public ResponseEntity<List<LearningContent>> getLearningContentsByUserId(@PathVariable String userId) {
-        List<LearningContent> learningContents = userService.getLearningContentsByUserId(userId);
-        return ResponseEntity.ok(learningContents);
+//    @GetMapping("/{userId}/learning-contents")
+//    public ResponseEntity<List<LearningContent>> getLearningContentsByUserId(@PathVariable String userId) {
+//        List<LearningContent> learningContents = userService.getLearningContentsByUserId(userId);
+//        return ResponseEntity.ok(learningContents);
+//    }
+
+    @PutMapping("/user/setAge")
+    public void setUserAge(@RequestHeader(name = "X-UserId") String userId, @RequestBody Map<String, Integer> ageMap){
+        log.info("set user age!");
+        userService.setUserAge(userId, ageMap.get("age"));
+    }
+    @PutMapping("/user/setNation")
+    public void setUserNation(@RequestHeader(name = "X-UserId") String userId, @RequestBody Map<String, String> nationMap){
+        log.info("set user nation!");
+        userService.setUserNation(userId, nationMap.get("nation"));
+    }
+    @PutMapping("/user/setLanguage")
+    public void setUserLanguage(@RequestHeader(name = "X-UserId") String userId, @RequestBody Map<String,String> languageMap){
+        log.info("set user language!");
+        userService.setUserLanguage(userId, languageMap.get("language"));
+    }
+    @PutMapping("/user/setGender")
+    public void setUserGender(@RequestHeader(name = "X-UserId") String userId, @RequestBody Map<String,String> genderMap){
+        log.info("set user gender!");
+        userService.setUserGender(userId, genderMap.get("gender"));
     }
 }
--- src/main/java/HiK/HiKServer/User/service/UserService.java
+++ src/main/java/HiK/HiKServer/User/service/UserService.java
@@ -5,12 +5,14 @@ import HiK.HiKServer.LearningContents.dto.LearningContentDto;
 import HiK.HiKServer.LearningContents.repository.LearningContentRepository;
 import HiK.HiKServer.User.domain.User;
 import HiK.HiKServer.User.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class UserService {
     @Autowired
@@ -31,8 +33,29 @@ public class UserService {
         }
         return learningContentDtos;
     }
-    public List<LearningContent> getLearningContentsByUserId(String userId) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        return user.getLearningContentList();
+
+    public void setUserAge(String userId, int age){
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setAge(age);
+        log.info("user set age: ", user.getAge());
     }
+    public void setUserNation(String userId, String nation){
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setNation(nation);
+        log.info("user set nation: ", user.getNation());
+    }
+    public void setUserLanguage(String userId, String language){
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setLanguage(language);
+        log.info("user set language: ", user.getLanguage());
+    }
+    public void setUserGender(String userId, String gender){
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setGender(gender);
+        log.info("user set gender: ", user.getGender());
+    }
+//    public List<LearningContent> getLearningContentsByUserId(String userId) {
+//        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+//        return user.getLearningContentList();
+//    }
 }
