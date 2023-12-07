d5fb03292eefa623f423ec31e0a98a8e869dcbef
bbx8216 <bbx8216@gmail.com>
2023-12-08 03:03:09 +0900

    fix: online-chatting 프롬프트 수정

--- src/main/java/HiK/HiKServer/gpt/PromptHandler_OnlineChatting.java
+++ src/main/java/HiK/HiKServer/gpt/PromptHandler_OnlineChatting.java
@@ -16,12 +16,12 @@ public class PromptHandler_OnlineChatting extends PromptHandler {
             "- You should be done for natural translation that can be easily understood by any Korean.\n" +
             "- It is a situation in which you meet friend, elder, younger, stranger or other people in online chatting and talk to them.\n" +
             "- If the listener is a friend and the intimacy is 1, you have to use honorifics.\n" +
-            "- If the listener is a friend and the intimacy is 2, you have to use half honorifics and use emoticons such as \"\uD83D\uDE0A\".\n" +
-            "- If the listener is a friend and the intimacy is 3, you have to speak comfortably and add \"ㅋㅋㅋㅋㅋㅋㅋ\".\n" +
+            "- If the listener is a friend and the intimacy is 2, you have to use half honorifics. Additionally the sentence is happy use the emoticon like \"\uD83D\uDE0A\" or \"ㅎㅎ\" and if the sentence is sad just use half honorifics or you can use \"ㅠㅠ\".\n" +
+            "- If the listener is a friend and the intimacy is 3, you have to speak comfortably. Additionally the sentence is funny you can add \"ㅋㅋㅋㅋㅋㅋㅋㅋㅋ\". And the sentence is sad you can use \"ㅠㅠ\".\n" +
             "- If the listener is a elder and the intimacy is 1 or 2 or 3, you have to speak politely.\n" +
             "- If the listener is a younger and the intimacy is 1, you have to speak honorifics.\n" +
-            "- If the listener is a younger and the intimacy is 2, you have to speak half honorifics use emoticons such as \"\uD83D\uDE0A\".\n" +
-            "- If the listener is a younger and the intimacy is 3, you have to speak comfortably and add \"ㅋㅋㅋㅋㅋㅋㅋ\".\n" +
+            "- If the listener is a younger and the intimacy is 2, you have to speak half honorifics. Additionally the sentence is happy use the emoticon like \"\uD83D\uDE0A\" or \"ㅎㅎ\" and if the sentence is sad just use half honorifics or you can use \"ㅠㅠ\".\n" +
+            "- If the listener is a younger and the intimacy is 3, you have to speak comfortably. Additionally the sentence is funny you can add \"ㅋㅋㅋㅋㅋㅋㅋㅋㅋ\". And the sentence is sad you can use \"ㅠㅠ\".\n" +
             "- If the listener is a stranger and the intimacy is 1 or 2 or 3, you have to speak honorifics.\n" +
             "- If the listener is not a friend, elder, younger and stranger you should use honorifics.\n" +
             "- In the course of the conversation, we have to change the style according to each listener and intimacy when a sentence comes in.\n" +
@@ -30,25 +30,32 @@ public class PromptHandler_OnlineChatting extends PromptHandler {
             "[Example]\n" +
             "<Input>\n" +
             "sentence: \"How old are you?\"\n" +
+            "Case 1) Filter - location: online-chatting, listener: friend, intimacy: 1\n" +
             "<Output>\n" +
-            "Case 2) Filter - location: online-chatting, listener: friend, intimacy: 1\n" +
             "\"몇살인가요?\"\n" +
-            "Case 3) Filter - location: online-chatting, listener: friend, intimacy: 2\n" +
-            "\"몇살이야?\uD83D\uDE0A\"\n" +
-            "Case 4) Filter - location: online-chatting, listener: friend, intimacy: 3\n" +
-            "\"ㅋㅋㅋㅋㅋㅋㅋㅋㅋ몇살임?\"\n" +
-            "Case 10) Filter - location: online-chatting, listener: elder, intimacy: 1 or 2 or 3\n" +
+            "Case 2) Filter - location: online-chatting, listener: friend, intimacy: 2\n" +
+            "<Output>\n" +
+            "\"몇살이야?\"\n" +
+            "Case 3) Filter - location: online-chatting, listener: friend, intimacy: 3\n" +
+            "<Output>\n" +
+            "\"몇살임?\"\n" +
+            "Case 4) Filter - location: online-chatting, listener: elder, intimacy: 1 or 2 or 3\n" +
+            "<Output>\n" +
             "\"연세가 어떻게 되시나요?\"\n" +
-            "Case 11) Filter - location: online-chatting, listener: younger, intimacy: 1\n" +
+            "Case 5) Filter - location: online-chatting, listener: younger, intimacy: 1\n" +
+            "<Output>\n" +
             "\"몇살인가요?\"\n" +
-            "Case 12) Filter - location: online-chatting, listener: younger, intimacy: 2\n" +
-            "\"몇살이야?\uD83D\uDE0A\"\n" +
-            "Case 13) Filter - location: online-chatting, listener: younger, intimacy: 3\n" +
-            "\"ㅋㅋㅋㅋㅋㅋㅋㅋㅋ몇살임?\"\n" +
-            "Case 14) Filter - location: online-chatting, listener: stranger, intimacy: 1 or 2 or 3\n" +
+            "Case 6) Filter - location: online-chatting, listener: younger, intimacy: 2\n" +
+            "<Output>\n" +
+            "\"몇살이야?\"\n" +
+            "Case 7) Filter - location: online-chatting, listener: younger, intimacy: 3\n" +
+            "<Output>\n" +
+            "\"몇살임?\"\n" +
+            "Case 8) Filter - location: online-chatting, listener: stranger, intimacy: 1 or 2 or 3\n" +
+            "<Output>\n" +
             "\"몇살인가요?\"\n" +
             "\n" +
-            "Ensure that your translations perfectly adhere to these [Instructions].";
+            "Ensure that your translations perfectly adhere to these [Instructions].\n";
     public void handleRequest(GptPrompt gptPrompt, String request_place, String request_listener) {
         boolean isExist = Arrays.stream(online_chatting_set).anyMatch(request_place::equals);
         if (isExist) {
